9a300392c1aaa27ee896436a6aaf5fba22193387
Muhammad Ahsan <ahsansoftengineer>
2021-01-20 14:59:49 +0500

    Part 39 Reactive Form Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component,  } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ export class AppComponent {
   onSubmit(data){
     console.log(data);
   }
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+  get email() {return this.loginForm.get('email')};
+  get password() {return this.loginForm.get('password')};
 }
